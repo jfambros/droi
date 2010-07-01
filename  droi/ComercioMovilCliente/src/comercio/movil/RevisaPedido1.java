b3c843fc11982ec3336dcbc7279ee4103a3cf5db
jfambros <jfambros@12eb78b7-5b31-db8a-7ba9-cf91819b93e5>
2010-07-01 18:07:14 +0000

    prueba pedido 1

---  droi/ComercioMovilCliente/src/comercio/movil/RevisaPedido1.java
+++  droi/ComercioMovilCliente/src/comercio/movil/RevisaPedido1.java
@@ -1,7 +1,12 @@
 package comercio.movil;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class RevisaPedido1 extends Activity{
 
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.revisapedido1);
+	}
 }
