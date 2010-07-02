a3eb8d866a555b772fb6953ea7b7c198a7fc045c
jfambros <jfambros@12eb78b7-5b31-db8a-7ba9-cf91819b93e5>
2010-07-02 08:36:00 +0000

    comienzo 2 de revisa pedido

---  droi/ComercioMovilCliente/src/comercio/movil/RevisaPedido1.java
+++  droi/ComercioMovilCliente/src/comercio/movil/RevisaPedido1.java
@@ -2,11 +2,33 @@ package comercio.movil;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class RevisaPedido1 extends Activity{
+	private EditText tvComentario = null;
+	private ImageView ivContinuar = null;
 
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.revisapedido1);
+        
+        //objetos
+        tvComentario = (EditText) findViewById(R.id.etComentarioRevisaPed1);
+        
+        ivContinuar = (ImageView) findViewById(R.id.ivContinuarRevisaPed1);
+        ivContinuar.setOnClickListener(ivContinuarPres);
+
 	}
+	
+	private OnClickListener ivContinuarPres = new OnClickListener() {
+		
+		public void onClick(View arg0) {
+			Log.i("comentario",tvComentario.getText().toString());
+		}
+	};
+
 }
