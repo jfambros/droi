ce6f682dfd4325a003889b77a234685f8e65432e
jfambros <jfambros@12eb78b7-5b31-db8a-7ba9-cf91819b93e5>
2010-12-07 02:04:46 +0000

    tienda

---  droi/ComercioMovilCliente/src/utils/Valores.java
+++  droi/ComercioMovilCliente/src/utils/Valores.java
@@ -2,10 +2,12 @@ package utils;
 
 public class Valores {
 	//Internet
-	public static String HOST = "tiendav.webcindario.com"; 
+	//public static String HOST = "tiendav.webcindario.com"; 
 	 
 	//equipo local
 	//public static String HOST = "10.0.2.2"; 
 
-	
+	//IP
+	public static String HOST = "192.168.1.102"; 
+	//public static String HOST = "amber";
 }
